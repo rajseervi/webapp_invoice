5329691f2544a3aa39bc1630dcfd0a1f65309515
rajseervi <41483452+rajseervi@users.noreply.github.com>
2025-05-02 19:55:08 +0530

    Delete src directory

--- src/utils/dateUtils.js
+++
@@ -1,7 +0,0 @@
-export const formatDate = (date) => {
-  return new Date(date).toLocaleDateString();
-};
-
-export const getCurrentDate = () => {
-  return new Date().toISOString().split('T')[0];
-};
